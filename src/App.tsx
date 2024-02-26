cbecc64135b48cc2371237863e86f1f34de2f75e
Manuel Venegas <mengasis@outlook.com>
2024-02-25 22:46:08 -0300

    feat: added initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,85 @@
 function App() {
   return (
     <div>
+      <img src="https://" alt="main"/>
+
+      <h1>Simple Omelette Recipe</h1>
+
+      <span>An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.</span>
+    
+    <div>
+      <span>Preparation time</span>
+      <ul>
+        <li>
+          <span>Total:</span> Approximatly 10 minutes.
+        </li>
+        <li>
+          <span>Preparation:</span> 5 minutes.
+        </li>
+        <li>
+          <span>Cooking:</span> 5 minutes.
+        </li>
+      </ul>
+    </div>
+
+    <h2>Ingredients</h2>
+
+    <ul>
+      <li>2-3 large eggs</li>
+      <li>Salt, to taste</li>
+      <li>Papper, to taste</li>
+      <li>1 tablespoon of butter or oil</li>
+      <li>Optional fillings: cheese, diced vegetables, cooked meats, herbs</li>
+    </ul>
+
+    <div />
+
+    <h2>Instructions</h2>
+
+    <ul>
+      <li>
+        <span>Beat the eggs:</span> In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture.
+      </li>
+      <li>
+        <span>Heat the pan:</span> Place a non-stick frying pan over medium heat and add butter or oil.
+      </li>
+      <li>
+        <span>Cook the omelette:</span> Once the butter is melted and bubbling, pour in the eggs.
+Tilt the pan to ensure the eggs evenly coat the surface.
+      </li>
+
+      <li>
+        <span>Add fillings (optional):</span> When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette.
+      </li>
+    </ul>
+
+    <div />
+
+    <h2>Nutrition</h2>
+
+    <span>The table below shows nutritional values per serving without the additional fillings.</span>
+
+    <div>
+      <ul>
+        <li>
+          <span>Calories</span>
+          <span>277kcal</span>
+        </li>
+        <li>
+          <span>Carbs</span>
+          <span>0g</span>
+        </li>
+        <li>
+          <span>Protein</span>
+          <span>20g</span>
+        </li>
+        <li>
+          <span>Fat</span>
+          <span>22g</span>
+        </li>
+      </ul>
+    </div>
+
     </div>
   );
 }
